a3b82afcfd3b3fd5c718b5695d7b5424f9c7b3c8
Dominic Tarr <dominic.tarr@gmail.com>
2018-04-09 16:13:48 -0700

    run tests both async and sync

--- test.js
+++ test.js
@@ -1,2 +1,14 @@
 
-process.exit(require('chloride-test')(require('./index')).fail)
+var sodium = require('./index')
+
+var fail = require('chloride-test')(sodium).fail
+
+if(fail) process.exit(fail)
+
+//run again, after libsodium wasm has had time to load
+setTimeout(function () {
+  process.exit(require('chloride-test')(sodium).fail)
+}, 200)
+
+
+
