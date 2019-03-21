4400326fbaba3a0e8f9ee2911f370d38022d3464
Dominic Tarr <dominic.tarr@gmail.com>
2019-03-22 07:46:08 +1300

    wrap methods, so that the reference doesn't change after loading wasm, fixes dominictarr/chloride#10

--- index.js
+++ index.js
@@ -3,12 +3,19 @@
 //to get automatic fallbacks!
 
 //load tweetnacl first, so that it works sync, and everything is there.
-module.exports = require('sodium-browserify-tweetnacl')
+var exports = require('sodium-browserify-tweetnacl')
+
+for(var k in exports) (function (fn, k) {
+  if('function' == typeof fn)
+    module.exports[k] = function () {
+      return fn.apply(this, [].slice.call(arguments))
+    }
+})(exports[k], k)
 
 //now load wasm which has to be async, ugh.
 var libsodium = require('libsodium-wrappers')
 libsodium.ready.then(function (value, what) {
-  require('./browser') (libsodium, module.exports)
+  require('./browser') (libsodium, exports)
 }).catch(function (err) {
   //escape from promise land, ugh
   setTimeout(function () {
@@ -16,6 +23,9 @@ libsodium.ready.then(function (value, what) {
     process.exit(1)
   })
 })
+
+
+
 
 
 
