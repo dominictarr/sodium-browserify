fd3087f2fa68b1328a2974cc77c51cd9af3aacdd
Dominic Tarr <dominic.tarr@gmail.com>
2019-03-22 07:40:36 +1300

    check that reference to a function doesn't change, fails for dominictarr/chloride#10

--- test.js
+++ test.js
@@ -1,12 +1,17 @@
 
 var sodium = require('./index')
 
+console.log(sodium)
+var hash = sodium.crypto_scalarmult
+
 var fail = require('chloride-test')(sodium).fail
 
 if(fail) process.exit(fail)
 
 //run again, after libsodium wasm has had time to load
 setTimeout(function () {
+  if(hash != sodium.crypto_scalarmult)
+    throw new Error('sodium.crypto_scalarmult has changed')
   process.exit(require('chloride-test')(sodium).fail)
 }, 200)
 
